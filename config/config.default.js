37d6f6d4186544d4b78c02e5f3d97aa1ac27cc26
wx1988 <413307928@qq.com>
2019-10-29 11:46:27 +0800

    重建 git

--- config/config.default.js
+++ config/config.default.js
@@ -28,7 +28,10 @@ module.exports = appInfo => {
       dialect: 'mysql',
       host: '127.0.0.1',
       port: 3306,
-      database: 'wstore',
+      database: 'wshop',
+      username: 'root',
+      password: 'root',
+
     };
 
   return {
